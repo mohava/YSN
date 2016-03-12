dfe6414e2c548e1a65ab05526311237f81730eb0
LittleEndu <enduslittlecomputer@gmail.com>
2016-03-12 16:13:27 +0200

    üritan txt file lugeda

--- master/Testing.java
+++ master/Testing.java
@@ -1,9 +1,20 @@
 package master;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by ATM on 12/03/2016.
  */
 //MIHKEL TESTIB
 //hOI, im temmie -LE
 public class Testing {
+	public static void main(String[] args) throws FileNotFoundException{
+		File file = new File(System.getProperty("user.dir")+"\\testfile.txt");
+		Scanner sc= new Scanner(file, "UTF-8");
+		while (sc.hasNextLine())
+			System.out.println(sc.nextLine());
+		sc.close();
+	}
 }
